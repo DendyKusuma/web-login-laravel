fcd9c64ecd8b536d9004bb311982919e14f6509a
Dendy Mulya Kusuma <dendy.mkusuma@gmail.com>
2023-10-07 21:07:27 +0700

    first commit

---
+++ resources/views/nama.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Ayo Ngoding - Yayasan Dar El Ilmi</title>
+</head>
+<body>
+  <h1>Ayo Ngoding</h1>
+  <p>Test</p>
+  <h1>....................</h1>
+</body>
+</html>
