7f0d4826d7a4853b9c0e6f75299e60e69875c6d6
Dendy Mulya Kusuma <dendy.mkusuma@gmail.com>
2023-10-13 13:56:52 +0700

    penambahan fiture baru

--- resources/views/nama.blade.php
+++ resources/views/nama.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Ayo Ngoding - Yayasan Dar El Ilmi</title>
+  <title>Ayo Ngoding - .. Yayasan Dar El Ilmi</title>
 </head>
 <body>
   <h1>Ayo Ngoding</h1>
